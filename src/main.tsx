ebb64eed696ee10f006263da99c566e2e73edb5e
na <na>
2024-10-03 17:10:22 -0700

    generate a simple createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import './output.css'
 
+
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+];
+);
+
+export default router
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
