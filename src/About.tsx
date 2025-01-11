454860764ca3eb0795d7ba9b7c2262701416716f
George Balch <gbalch714@gmail.com>
2025-01-10 17:16:35 -0800

    Change about

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>I am a former scientist who dropped out of academia to go live in the mountains.</div>
+    <div>I am a former scientist who dropped out of academia to go live in the mountains.  I spend the majority of my time trying to engineer intelligence in machines and some time on philosophy.</div>
   )
 }
 
