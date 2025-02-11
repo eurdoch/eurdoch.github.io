0ccf038522651febceee5b1a1ea34f95dc1b0313
George Balch <gbalch714@gmail.com>
2025-02-11 14:14:30 -0800

    Change to 50% width and remove header background color

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,20 @@ import SocialIcon from './SocialIcon';
 
 function App() {
   return (
-    <div className="h-screen w-screen text-gray-600">
-      <div className="h-20 text-2xl w-screen bg-slate-100 p-8 flex gap-4 items-center justify-center">
-        <div className="font-bold">
-          <Link to="/" className="hover:text-black">eurdoch</Link>
-        </div>
-        {/*<div className="ml-auto">*/}
-        {/*  <Link to="/about" className="hover:text-black">about</Link>*/}
-        {/*</div>*/}
-        {/*<div>
-          <Link to="/blog" className="hover:text-black">blog</Link>
-        </div>*/}
+    <div className="h-screen flex justify-center">
+      <div className="w-1/2 text-gray-600">
+        <div className="h-20 text-2xl w-full p-8 flex gap-4 items-center justify-center">
+          <div className="font-bold">
+            <Link to="/" className="hover:text-black">eurdoch</Link>
+          </div>
 
-        <div className="ml-auto" />
-        <SocialIcon icon={faXTwitter} href="https://x.com/foomprep" size='sm' hoverColor='black' />
-        <SocialIcon icon={faGithub} href="https://github.com/foomprep" size='sm' hoverColor='black' />
-      </div>
-      <div className="p-4">
-        <Outlet />
+          <div className="ml-auto" />
+          <SocialIcon icon={faXTwitter} href="https://x.com/foomprep" size='sm' hoverColor='black' />
+          <SocialIcon icon={faGithub} href="https://github.com/foomprep" size='sm' hoverColor='black' />
+        </div>
+        <div className="p-4">
+          <Outlet />
+        </div>
       </div>
     </div>
   )
